957c30a1061875e7227ffedfe6c04682f1398ca6
ishanmd916 <prrmnjl@gmail.com>
2025-12-12 14:16:04 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+const fs = require("fs");
+if (fs.existsSync("config.env"))
+  require("dotenv").config({ path: "./config.env" });
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+function convertToBool(text, fault = "true") {
+  return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~hiTxSiDKYOX1EbH",
-ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/XkfKhcZ2/9766.jpg",
-ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃 `PROGRAMER ISHAN MD` 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - :  `PROGRAMER ISHAN MD´",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
+  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
 };
